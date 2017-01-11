8581075e827dbd9bff26d2eeda8f905b236d6a39
wencakisa <wencakisa@gmail.com>
2017-01-11 17:59:45 +0200

    Added default mongodb connection with mongoose

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 let express = require('express')
+let mongoose = require('mongoose')
 
+mongoose.Promise = global.Promise
 let app = express()
+
+const connection = 'mongodb://localhost:27017/express-db'
 const port = 1337
 
 app.get('/', (req, res) => {
-  res.send('Welcome home.')
+  mongoose
+    .connect(connection)
+    .then(() => {
+      console.log('Mongo DB is ready.')
+      res.send('Welcome home.')
+    })
 })
 
 app.listen(port)
