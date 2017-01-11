a646a6f4fe807c450b0d74edc37c99c359f8e6f9
wencakisa <wencakisa@gmail.com>
2017-01-11 20:36:50 +0200

    Added some auth dependencies

--- server/config/express.js
+++ server/config/express.js
@@ -1,9 +1,23 @@
 let express = require('express')
+let cookieParser = require('cookie-parser')
+let bodyParser = require('body-parser')
+let session = require('express-session')
+let passport = require('passport')
 let path = require('path')
 
 module.exports = (app, config, viewsDir) => {
   app.set('view engine', 'handlebars')
   app.set('views', path.join(config.rootPath, viewsDir))
+
+  app.use(cookieParser())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(session({
+    secret: 'neshto-taino!#?@$%',
+    resave: true,
+    saveUninitialized: true
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
   app.use('/public', express.static(path.join(config.rootPath, 'public')))
 
   console.log('Express.js is ready.')
