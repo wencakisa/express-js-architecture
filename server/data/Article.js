35e0971852b4ea809adb99890a55b57e3996e147
wencakisa <wencakisa@gmail.com>
2017-01-13 11:41:33 +0200

    Added min/max len validation

--- server/data/Article.js
+++ server/data/Article.js
@@ -1,13 +1,26 @@
 let mongoose = require('mongoose')
 
 let requiredValidationMessage = '{PATH} is required.'
+let minLengthValidationMessage = 'The value of path `{PATH}` (`{VALUE}`) is shorter than the minimum allowed length ({MINLENGTH}).'
+let maxLengthValidationMessage = 'The value of path `{PATH}` (`{VALUE}`) is longer than the maximum allowed length ({MAXLENGTH}).'
+
+let titleMinLength = [5, minLengthValidationMessage]
+let titleMaxLength = [30, maxLengthValidationMessage]
+let descriptionMaxLength = [500, maxLengthValidationMessage]
 
 let articleSchema = mongoose.Schema({
   title: {
-    type: String, required: requiredValidationMessage, unique: true
+    type: String,
+    required: requiredValidationMessage,
+    unique: true,
+    minlength: titleMinLength,
+    maxlength: titleMaxLength
   },
   description: {
-    type: String, required: requiredValidationMessage
+    type: String,
+    required: requiredValidationMessage,
+    minlength: titleMinLength,
+    maxlength: descriptionMaxLength
   },
   authorUsername: String
 })
